70999436ca8025b80a1559bff1ef9a14a476a7c7
tigerisaac <isaac.cw.zhang@gmail.com>
2025-02-22 21:20:30 -0600

    okay hope this is the last time pypi stuff

--- src/wifi_monitoring_util/log.py
+++ src/wifi_monitoring_util/log.py
@@ -1,1 +1,4 @@
-log_file_path = "tests/testlog2.txt"
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Get package directory
+log_file_path = os.path.join(BASE_DIR, "tests", "testlog2.txt") 
